70dfd484a12dd74059b7ed309a8f64b2aac44cb0
Cyril SIX <cyril.six@kalray.eu>
2019-09-23 17:10:32 +0200

    Test harness

--- matchmaking.py
+++ matchmaking.py
@@ -41,21 +41,21 @@ PlayerAssign = Tuple[Day, Table]
 Solution = Dict[Name, PlayerAssign]
 
 
-def check_solution(parseInfo: ParseInfo, solution: Solution, rankDiff: Optional[int] = None) -> bool:
+def _check_solution(parseInfo: ParseInfo, solution: Solution, rankDiff: Optional[int] = None) -> int:
     # 1) All players that are available for a day, should have an assignment
     for player in parseInfo:
         _, daysOk = parseInfo[player]
         if not any(daysOk):
             continue
         if player not in solution:
-            return False
+            return -1
 
     # 2) Each player should be available during their assigned day
     for player in solution:
         _, daysOk = parseInfo[player]
         day, _ = solution[player]
         if not daysOk[day]:
-            return False
+            return -2
 
     # 3) Each table should have 4 to 6 players
     tableSize: Dict[Table, int] = {}
@@ -66,7 +66,7 @@ def check_solution(parseInfo: ParseInfo, solution: Solution, rankDiff: Optional[
         tableSize[table] += 1
     for table in tableSize:
         if not (4 <= tableSize[table] <= 6):
-            return False
+            return -3
 
     # 4) Check that the rank difference of a table does not exceed RankDiff
     if rankDiff:
@@ -89,17 +89,47 @@ def check_solution(parseInfo: ParseInfo, solution: Solution, rankDiff: Optional[
             rankRange = tableRankRange[table]
             tableDiff = rankRange[1] - rankRange[0]
             if tableDiff > rankDiff:
-                return False
+                return -4
 
-    return True
+    return 0
+
+
+def check_solution(parseInfo: ParseInfo, solution: Solution, rankDiff: Optional[int] = None) -> bool:
+    return (_check_solution(parseInfo, solution, rankDiff) == 0)
+
+
+def test_check_solution() -> None:
+    parseInfo = {"toto": (0., [False, True]), "titi": (0., [False, True]), "tata": (0., [False, True]), "lolo": (0., [True, True])}
+
+    solution = {"toto": (1, 0), "titi": (1, 0), "tata": (1, 0), "lolo": (1, 0)}
+    assert(_check_solution(parseInfo, solution, 0) == 0) # success
+
+    solution = {"toto": (1, 0), "tata": (1, 0), "lolo": (1, 0)}
+    assert(not _check_solution(parseInfo, solution, 0) == -1) # fail 1)
+
+    solution = {"toto": (0, 0), "titi": (0, 0), "tata": (0, 0), "lolo": (0, 0)}
+    assert(not _check_solution(parseInfo, solution, 0) == -2) # fail 2)
+
+    parseInfo = {"toto": (0., [False, True]), "titi": (0., [False, True]), "tata": (0., [False, True])}
+    solution = {"toto": (1, 0), "titi": (1, 0), "tata": (1, 0)}
+    assert(not _check_solution(parseInfo, solution, 0) == -3) # fail 3)
+
+    parseInfo = {"toto": (10., [False, True]), "titi": (0., [False, True]), "tata": (0., [False, True]), "lolo": (0., [True, True])}
+    solution = {"toto": (1, 0), "titi": (1, 0), "tata": (1, 0), "lolo": (1, 0)}
+    assert(not _check_solution(parseInfo, solution, 0) == -4) # fail 4)
+
+    print("All good!")
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
 args = parser.parse_args()
 
-with open(args.file, 'r') as f:
-    parseInfo = parse_file(f)
+if args.file == "test":
+    test_check_solution()
+else:
+    with open(args.file, 'r') as f:
+        parseInfo = parse_file(f)
 
-print(parseInfo)
+    print(parseInfo)
 
