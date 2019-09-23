1c5e410f71bbc59ad3068633870ab9f0ca34a4af
Cyril SIX <cyril.six@kalray.eu>
2019-09-23 11:51:40 +0200

    ParseInfo datatype

--- matchmaking.py
+++ matchmaking.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3.6
 import argparse
+from typing import *
+
+class PlayerInfo:
+  def __init__(self, score: int, daysOk: List[bool]):
+    self.score = score
+    self.daysOk = daysOk
+
+Name = str
+ParseInfo = Dict[Name, PlayerInfo]
+def parse (filename: str) -> ParseInfo:
+  return {}
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
 args = parser.parse_args()
 
-print(args.file)
+print(parse(args.file))
